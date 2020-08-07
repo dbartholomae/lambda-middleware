ee1c50297e40e6c9ea24a0e063a0da61591164d7
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-07 17:32:10 +0200

    feat(json-serializer): add debug messages

--- packages/json-serializer/src/JsonSerializer.ts
+++ packages/json-serializer/src/JsonSerializer.ts
@@ -5,14 +5,17 @@ import { PromiseHandler } from './interfaces/PromiseHandler'
 const logger: IDebugger = debugFactory('@lambda-middleware/json-serializer')
 
 export const jsonSerializer = <E>() => (handler: PromiseHandler<E, any>) => async (event: E, context: Context): Promise<APIGatewayProxyResult> => {
-  logger('')
+  logger('Running handler')
   const response = await handler(event, context)
+  logger('Response received')
   if (response === undefined) {
+    logger('Undefined response, returning statusCode 204')
     return {
       statusCode: 204,
       body: ''
     }
   }
+  logger('Response is not undefined, returning response and statusCode 200')
   return {
     statusCode: 200,
     body: JSON.stringify(response)
