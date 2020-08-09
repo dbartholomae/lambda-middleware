7d513804cc6216c181178b28080902697c67c326
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-10 01:45:17 +0200

    chore: fix linting setup

--- packages/ie-no-open/.eslintrc.js
+++ packages/ie-no-open/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
