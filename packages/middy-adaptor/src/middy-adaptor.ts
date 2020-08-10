4401d0a82aef7da6a62c546b7ce03cb5f3de43b2
Daniel Bartholomae <daniel@bartholomae.name>
2020-08-10 14:02:49 +0200

    feat(middy-adaptor): add logging

--- packages/middy-adaptor/src/middy-adaptor.ts
+++ packages/middy-adaptor/src/middy-adaptor.ts
@@ -1,4 +1,4 @@
-// import debugFactory, { IDebugger } from 'debug'
+import debugFactory, { IDebugger } from "debug";
 import { PromiseHandler } from "@lambda-middleware/utils";
 import { Context } from "aws-lambda";
 import {
@@ -8,7 +8,7 @@ import {
 } from "./interfaces/MiddyTypes";
 import { promisifyMiddyMiddleware } from "./utils/promisifyMiddyMiddleware";
 
-// const logger: IDebugger = debugFactory('@lambda-middleware/no-sniff')
+const logger: IDebugger = debugFactory("@lambda-middleware/middy-adaptor");
 
 export const middyAdaptor = <Event>(
   middyMiddleware: MiddlewareObject<unknown, unknown>
@@ -21,10 +21,12 @@ export const middyAdaptor = <Event>(
   let callbackResponse: unknown = undefined;
 
   const callback = (error: unknown, result: unknown) => {
+    logger("instance callback function called");
     callbackCalled = true;
     callbackError = error;
     callbackResponse = result;
   };
+
   const instance: Instance = {
     context: { ...context },
     event: { ...event },
@@ -41,9 +43,13 @@ export const middyAdaptor = <Event>(
     );
   }
 
+  logger("Checking for before middleware");
   if (promisifiedMiddyMiddleware.before !== undefined) {
+    logger("Calling before middleware");
     await promisifiedMiddyMiddleware.before(instance);
+    logger("before middleware called");
     if (callbackCalled) {
+      logger("callback called in before middleware");
       if (callbackError) {
         throw callbackError;
       }
@@ -51,25 +57,36 @@ export const middyAdaptor = <Event>(
     }
   }
   try {
+    logger("Calling handler");
     instance.response = await handler(instance.event, context);
+    logger("handler ran successfully");
+    logger("Checking for after middleware");
     if (promisifiedMiddyMiddleware.after !== undefined) {
+      logger("Calling after middleware");
       await promisifiedMiddyMiddleware.after(instance);
+      logger("after middleware called");
     }
   } catch (error) {
+    logger("error in handler or before or after middleware");
     instance.error = error;
     let newError = error;
+    logger("Checking for onError middleware");
     if (promisifiedMiddyMiddleware.onError !== undefined) {
+      logger("Calling onError middleware");
       newError = await promisifiedMiddyMiddleware.onError(instance);
+      logger("onError middleware called");
     }
     if (newError) {
       throw newError;
     }
   }
   if (callbackCalled) {
+    logger("callback called in after or onError middleware");
     if (callbackError) {
       throw callbackError;
     }
     return callbackResponse;
   }
+  logger("returning response");
   return instance.response;
 };
