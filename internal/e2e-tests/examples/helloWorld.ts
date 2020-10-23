7798f646fbcbe25182a76b9ad161abfb5310d523
Daniel Bartholomae <daniel@bartholomae.name>
2020-10-23 15:50:27 +0200

    test: finish e2e test

--- internal/e2e-tests/examples/helloWorld.ts
+++ internal/e2e-tests/examples/helloWorld.ts
@@ -5,16 +5,17 @@ import { compose } from "@lambda-middleware/compose";
 import { cors } from "@lambda-middleware/cors";
 import { errorHandler } from "@lambda-middleware/http-error-handler";
 import { jsonSerializer } from "@lambda-middleware/json-serializer";
+import { EncryptionAlgorithms, jwtAuth } from "@lambda-middleware/jwt-auth";
 
 import { IsString } from "class-validator";
 
 // Define a validator for the body via class-validator
 class NameBody {
   @IsString()
-  public firstName: string = "";
+  public firstName: string;
 
   @IsString()
-  public lastName: string = "";
+  public lastName: string;
 }
 
 // This is your AWS handler
@@ -29,11 +30,25 @@ const wrapper = compose(
   // add cors headers last so even error responses from the
   // errorHandler middleware have cors headers applied
   cors(),
+
+  // errorHandler is next so errors from all other middlewares are caught
   errorHandler(),
+
+  // jwtAuth needs to be before jsonSerializer as it already returns
+  // a serialized response
+  jwtAuth({
+    algorithm: EncryptionAlgorithms.HS256,
+    secretOrPublicKey: "secret",
+  }),
+
+  // jsonSerializer needs to be below all middlewares that handle APIGatewayResponses
   jsonSerializer(),
+
+  // classValidator should be last as it replaces the body of the event
+  // and other middlewares might not be able to handle the modified event
   classValidator({
     bodyType: NameBody,
   })
-);
+)(helloWorld);
 
-export const handler = wrapper(helloWorld);
+export const handler = wrapper;
