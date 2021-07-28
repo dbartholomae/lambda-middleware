7d0ab74f9d88f8f0c61bc688fd9e2d1e397f3a4f
Matt Jenner <matt@jenner.me.uk>
2021-07-28 08:40:59 +0100

    fix(json-deserializer): fixed code-coverage
    Worked around issue with code-coverage in  RequestBodyNotJsonError.ts by adding istanbul ignore next comment and an explanation of why as-agreed.

--- packages/json-deserializer/src/customErrors/RequestBodyNotJsonError.ts
+++ packages/json-deserializer/src/customErrors/RequestBodyNotJsonError.ts
@@ -2,8 +2,12 @@ export class RequestBodyNotJsonError extends Error {
   body: string;
   innerError: Error;
 
+  // Ignoring the following constructor block due to how coverage is applied on certain transpiled ES5 code
+  // see here for more details about this issue: https://github.com/microsoft/TypeScript/issues/13029
+  /* istanbul ignore next */
   constructor(message: string, body: string, innerError: Error) {
     super(message);
+
     this.name = "RequestBodyNotJsonError";
     this.body = body;
     this.innerError = innerError;
