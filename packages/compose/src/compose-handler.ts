9822363ec74306b7b59c14f1792537b0bd3c1b16
czlowiek488 <34620109+czlowiek488@users.noreply.github.com>
2022-12-17 20:03:11 +0100

    feat: add easly accessable handler in order to make unit tests easier

--- packages/compose/src/compose-handler.ts
+++ packages/compose/src/compose-handler.ts
@@ -85,5 +85,7 @@ export function composeHandler<V, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
 export function composeHandler(...fns: Function[]): any {
   /* eslint-disable-next-line @typescript-eslint/ban-ts-ignore */
   // @ts-ignore
-  return compose(...fns.slice(0, fns.length - 1))(fns[fns.length - 1]);
+  const composed = compose(...fns.slice(0, fns.length - 1))(fns[fns.length - 1]);
+  composed.handler = fns[fns.length - 1];
+  return composed;
 }
